9f454407017d4f558ea1bc262ff9f64117893e43
unknown <victorst79@gmail.com>
2019-04-15 22:51:08 +0200

    server working

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,7 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 // VARIABLES
+const api_key = 'RGAPI-301b283a-41e0-493c-ac61-22ef21753988';
 var summoner;
 
 // FUNCTIONS
@@ -14,12 +15,12 @@ var summoner;
  * Obtiene los datos basicos sobre la cuenta de invocador
  */
 function get_summoner(summonerName){
-    request({url: 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ summonerName +'?api_key=RGAPI-9192060b-fc4e-4815-a2d5-e5da6a65c396', json: true}, function(err, res, json) {
+    request({url: 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ summonerName +'?api_key='+ api_key +'', json: true}, function(err, res, json) {
     if (err) {
         throw err;
     }
         console.log(json);
-        summoner = json;        
+        summoner = json;
     });
 }
 
@@ -34,6 +35,12 @@ function get_summoner(summonerName){
 // SOCKET.IO
 io.on('connection', function(socket){
     console.log("New Conexion");
-    get_summoner('Eliczika');
-    io.emit('user',JSON.stringify(summoner));
+
+    // RECIVE EL SUMMONER
+    socket.on('searchSummoner',function(data){
+        get_summoner(JSON.parse(data));
+
+        // DEVUELVE LOS DATOS DEL SUMMONER
+        io.emit('summonerData',JSON.stringify(summoner));
+    });
 });
