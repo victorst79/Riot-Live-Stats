fe12832b16ea839b783e0de16cfacbd6014d84f0
unknown <victorst79@gmail.com>
2019-04-16 22:05:40 +0200

    mejoras en el servidor, el cliente ahora no ve informacion directa del servidor

--- Server/server.js
+++ Server/server.js
@@ -9,10 +9,12 @@ var io = require('socket.io').listen(server);
 // VARIABLES
 const api_key = 'RGAPI-301b283a-41e0-493c-ac61-22ef21753988';
 var summoner;
+var match;
 
 // FUNCTIONS
 /**
  * Obtiene los datos basicos sobre la cuenta de invocador
+ * @param summonerName
  */
 function get_summoner(summonerName){
     request({url: 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ summonerName +'?api_key='+ api_key +'', json: true}, function(err, res, json) {
@@ -21,6 +23,20 @@ function get_summoner(summonerName){
     }
         console.log(json);
         summoner = json;
+    });
+}
+
+/**
+ * Obtiene los datos del match actual en base a la ID de summoner
+ */
+function get_match(summonerID){
+    
+    request({url: 'https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/'+ summonerID +'?api_key='+ api_key +'', json: true}, function(err, res, json) {
+    if (err) {
+        throw err;
+    }
+        console.log(json);
+        match = json;
     });
 }
 
@@ -40,7 +56,10 @@ io.on('connection', function(socket){
     socket.on('searchSummoner',function(data){
         get_summoner(JSON.parse(data));
 
+        
         // DEVUELVE LOS DATOS DEL SUMMONER
         io.emit('summonerData',JSON.stringify(summoner));
+        
+        console.log(match);
     });
 });
